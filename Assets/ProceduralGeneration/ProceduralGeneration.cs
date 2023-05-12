3c2082b90d8e9412b13e0c351b949b27a798ed9b
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-05-12 17:56:31 -0600

    Commented things

--- Assets/ProceduralGeneration/ProceduralGeneration.cs
+++ Assets/ProceduralGeneration/ProceduralGeneration.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Generates the layout of rooms
+/// </summary>
 public class ProceduralGeneration : MonoBehaviour
 {
+    // The size of the map
     [SerializeField] Vector2 mapSize;
 
+    // The tilemaps that can be chosen from
+    // TODO: Remove this
     [SerializeField] List<GameObject> possibleTilemaps;
 
+    // The room generation parameters
     [SerializeField] RoomGenerationParameters roomGenerationParameters;
 
+    // Default room that will be instantiated when generating
     [SerializeField] GameObject room;
 
+    // The instance
     public static ProceduralGeneration proceduralGenerationInstance { get; private set; }
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Sets up the singleton
+    /// </summary>
     void Awake()
     {
         if (proceduralGenerationInstance != null && proceduralGenerationInstance != this)
@@ -25,11 +36,18 @@ public class ProceduralGeneration : MonoBehaviour
         proceduralGenerationInstance = this;
     }
 
+    /// <summary>
+    /// Generates the rooms
+    /// </summary>
+
     void Start()
     {
         Generate();
     }
 
+    /// <summary>
+    /// Generates the rooms
+    /// </summary>
     void Generate()
     {
         for (int i = 0; i < mapSize.x; i++)
@@ -41,13 +59,18 @@ public class ProceduralGeneration : MonoBehaviour
                 Vector2 tileSize;
                 tileSize.x = tilemap.GetComponent<Grid>().cellSize.x;
                 tileSize.y = tilemap.GetComponent<Grid>().cellSize.y;
-                location.x = i * tileSize.x * room.GetComponent<Room>().GetSize().x;
-                location.y = j * tileSize.y * room.GetComponent<Room>().GetSize().y;
+                location.x = i * tileSize.x * room.GetComponent<Room>().size.x;
+                location.y = j * tileSize.y * room.GetComponent<Room>().size.y;
                 CreateRoom(tilemap, location);
             }
         }
     }
 
+    /// <summary>
+    /// Creates a room with the given tilemap and a given location
+    /// </summary>
+    /// <param name="tilemap"> The tilemap of the new room </param>
+    /// <param name="location"> The location of the new room </param>
     void CreateRoom(GameObject tilemap, Vector2 location)
     {
         GameObject newRoom = Instantiate(room, location, Quaternion.identity);
@@ -57,23 +80,38 @@ public class ProceduralGeneration : MonoBehaviour
         newRoom.SetActive(true);
     }
 
+    /// <summary>
+    /// Gets the current room generation parameters
+    /// </summary>
+    /// <returns> The room generation paramters </returns>
     public RoomGenerationParameters GetRoomGenerationParameters()
     {
         return roomGenerationParameters;
     }
 
+    /// <summary>
+    /// Changes the room generation paramters by the added amount
+    /// </summary>
+    /// <param name="addedRoomParams"> The amount to change the room generation parmaters by </param>
     public void AddRoomGenerationParameters(RoomGenerationParameters addedRoomParams)
     {
         roomGenerationParameters.Add(addedRoomParams);
     }
 }
 
+/// <summary>
+/// The parameters for room generation
+/// </summary>
 [System.Serializable]
 public class RoomGenerationParameters
 {
     [SerializeField] public int numEnemies;
     [SerializeField] public GameObject enemy;
 
+    /// <summary>
+    /// Adds another room generation paramters to this one
+    /// </summary>
+    /// <param name="other"> The other room generation parameters </param>
     public void Add(RoomGenerationParameters other)
     {
         numEnemies += other.numEnemies;
--- Assets/ProceduralGeneration/Room.cs
+++ Assets/ProceduralGeneration/Room.cs
@@ -3,52 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+/// <summary>
+/// The room, that handles enemy and trap generation when entered
+/// </summary>
 public class Room : MonoBehaviour
 {
-
+    // The tile map of this room; defines the shape of this room.
+    // TODO: Make this determined by the room size and a bitmap of directions
     [SerializeField] public GameObject tilemap;
+
+    // The dimensions of this room
     [SerializeField] public Vector2 size = new Vector2(11, 11);
 
+    // The collider that detects when this room has been entered
     BoxCollider2D roomBox;
 
+    // Whether or not this room has been generated
     bool generated = false;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Initializes the collision and the tilemap
+    /// </summary>
     void Start()
     {
         tilemap = Instantiate(tilemap, transform);
         tilemap.SetActive(true);
+
         roomBox = gameObject.AddComponent<BoxCollider2D>();
         roomBox.transform.parent = this.transform;
         roomBox.transform.position = this.transform.position;
         roomBox.size = new Vector3(size.x, size.y, 0);
-        Debug.Log(roomBox.size);
         roomBox.isTrigger = true;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public Vector2 GetSize()
-    {
-        return size;
-    }
-
-    public GameObject GetTilemap()
-    {
-        return tilemap;
-    }
-
+    /// <summary>
+    /// Generates the enemies and traps and other things that appear in a room
+    /// </summary>
     public void GenerateRoom()
     {
+        // Don't generate if already generated
         if (generated)
         {
             return;
         }
         generated = true;
+
         RoomGenerationParameters roomParams = ProceduralGeneration.proceduralGenerationInstance.GetRoomGenerationParameters();
         for (int i = 0; i < roomParams.numEnemies; i++)
         {
@@ -58,6 +57,10 @@ public class Room : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Handles when the player enters the room
+    /// </summary>
+    /// <param name="collision"> The collider that entered the trigger </param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == Player._instance.gameObject)
