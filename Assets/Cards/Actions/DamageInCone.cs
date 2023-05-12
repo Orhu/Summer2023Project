1bbde1d64979aa8aaf933e821b00900edb26ae33
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-05-12 12:09:33 -0600

    Action subclass comments

--- Assets/Cards/Actions/DamageInCone.cs
+++ Assets/Cards/Actions/DamageInCone.cs
@@ -4,26 +4,53 @@ using UnityEngine;
 
 namespace CardSystem.Effects
 {
+    /// <summary>
+    /// A scriptable object for storing data about a damage cone type.
+    /// </summary>
     [CreateAssetMenu(fileName = "NewDamageInCone", menuName = "Cards/Actions/DamageInCone")]
     public class DamageInCone : CardAction
     {
+        // The damage that will be dealt.
         public int damage = 1;
+        // The radius of the cone.
+        public float range = 6;
+        // The arc width in degrees of the cone.
+        public float arcWidth = 12;
+
+        /// <summary>
+        /// Gets the formated description of this card.
+        /// </summary>
+        /// <returns> The description with any Serialized Field names that appear in [] replaced with their actual value.</returns>
         public override string GetFormattedDescription()
         {
             return description.Replace("[Damage]", damage.ToString());
         }
 
-        public override void PreviewEffect()
+        /// <summary>
+        /// Starts rendering a preview of what this action will do.
+        /// </summary>
+        /// <param name="player"> The player that will be playing this action. </param>
+        public override void Preview(ICardPlayer player)
         {
-            Debug.Log("Hello World");
+
         }
-        public override void CancelPreview()
+
+        /// <summary>
+        /// Stops rendering a preview of what this action will do.
+        /// </summary>
+        /// <param name="player"> The player that will no longer be playing this action. </param>
+        public override void CancelPreview(ICardPlayer player)
         {
-            Debug.Log("Cancel World");
+
         }
-        public override void ConfirmPreview()
+
+        /// <summary>
+        /// Plays this action and causes all its effects. Also cancels any relevant previews.
+        /// </summary>
+        /// <param name="player"> The player that will be playing this action. </param>
+        public override void Play(ICardPlayer player)
         {
-            Debug.Log("Yes World");
+
         }
     }
 }
--- Assets/Cards/Actions/Projectile.cs
+++ Assets/Cards/Actions/Projectile.cs
@@ -4,27 +4,53 @@ using UnityEngine;
 
 namespace CardSystem.Effects
 {
+    /// <summary>
+    /// A scriptable object for storing data about a projectile type.
+    /// </summary>
     [CreateAssetMenu(fileName = "NewProjectile", menuName = "Cards/Actions/Projectile")]
     public class Projectile : CardAction
     {
+        // The damage this projectile will deal.
         public int damage = 1;
+        // The distance this projectile will travel.
         public float range = 6;
+        // The speed this projectile will travel at.
+        public float speed = 12;
+
+        /// <summary>
+        /// Gets the formated description of this card.
+        /// </summary>
+        /// <returns> The description with any Serialized Field names that appear in [] replaced with their actual value.</returns>
         public override string GetFormattedDescription()
         {
             return description.Replace("[Damage]", damage.ToString());
         }
 
-        public override void PreviewEffect()
+        /// <summary>
+        /// Starts rendering a preview of what this action will do.
+        /// </summary>
+        /// <param name="player"> The player that will be playing this action. </param>
+        public override void Preview(ICardPlayer player)
         {
-            Debug.Log("Hello World");
+
         }
-        public override void CancelPreview()
+
+        /// <summary>
+        /// Stops rendering a preview of what this action will do.
+        /// </summary>
+        /// <param name="player"> The player that will no longer be playing this action. </param>
+        public override void CancelPreview(ICardPlayer player)
         {
-            Debug.Log("Cancel World");
+
         }
-        public override void ConfirmPreview()
+
+        /// <summary>
+        /// Plays this action and causes all its effects. Also cancels any relevant previews.
+        /// </summary>
+        /// <param name="player"> The player that will be playing this action. </param>
+        public override void Play(ICardPlayer player)
         {
-            Debug.Log("Yes World");
+
         }
     }
 }
