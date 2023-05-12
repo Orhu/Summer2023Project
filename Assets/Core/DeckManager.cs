6924b264e34d08f297d0099ef02341d56b783cf4
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-05-12 12:23:59 -0600

    Deck Manger Comments

--- Assets/Core/DeckManager.cs
+++ Assets/Core/DeckManager.cs
@@ -3,26 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using CardSystem;
 
+/// <summary>
+/// Represents a deck of cards and a hand that cards can be previewed and played from.
+/// </summary>
 public class DeckManager : MonoBehaviour
 {
+    // Global singleton for the player's deck.
     public static DeckManager playerDeck;
+    // All the cards in the deck.
     public List<Card> cards;
+    // The side of this deck's hand.
     public int handSize = 3;
-
-    public delegate void deckChangedNotification();
-    public deckChangedNotification onCardDrawn;
-
+    // The cards in the draw pile.
     [System.NonSerialized]
     public List<Card> drawableCards;
+    // The cards in hand.
     [System.NonSerialized]
     public List<Card> inHandCards = new List<Card>();
-    [System.NonSerialized]
-    public List<int> previewedCardIndices = new List<int>();
-    [System.NonSerialized]
-    public Dictionary<int, float> cardIndicesToCooldowns = new Dictionary<int, float>();
+    // The cards in the discard pile.
     [System.NonSerialized]
     public List<Card> discardedCards = new List<Card>();
+    // The indices in the hand of the cards currently being previewed.
+    [System.NonSerialized]
+    public List<int> previewedCardIndices = new List<int>();
+    // The indices of the cards on cooldown mapped to the time remaining on the cooldown.
+    [System.NonSerialized]
+    public Dictionary<int, float> cardIndicesToCooldowns = new Dictionary<int, float>();
 
+    public delegate void deckChangedNotification();
+    // Called when a card is drawn.
+    public deckChangedNotification onCardDrawn;
+
+    /// <summary>
+    /// Initializes Singleton
+    /// </summary>
     private void Awake()
     {
         if (playerDeck == null)
@@ -36,6 +50,9 @@ public class DeckManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Initializes the draw pile and hand.
+    /// </summary>
     private void Start()
     {
         drawableCards = new List<Card>(cards);
@@ -46,6 +63,9 @@ public class DeckManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Updates all cooldowns and draws new cards when needed.
+    /// </summary>
     private void Update()
     {
         foreach (KeyValuePair<int, float> cardIndexToCooldown in new Dictionary<int, float>(cardIndicesToCooldowns))
@@ -65,7 +85,9 @@ public class DeckManager : MonoBehaviour
         }
     }
 
-
+    /// <summary>
+    /// Moves all cards from the discard pile to the draw pile and shuffles.
+    /// </summary>
     public void ReshuffleDrawPile()
     {
         while (drawableCards.Count > 0)
@@ -81,6 +103,47 @@ public class DeckManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Toggles the preview for the a card.
+    /// </summary>
+    /// <param name="handIndex"> The index in the hand of the card. </param>
+    public void TogglePreviewCard(int handIndex)
+    {
+        if (inHandCards.Count <= handIndex || cardIndicesToCooldowns.ContainsKey(handIndex))
+        {
+            return;
+        }
+        Card card = inHandCards[handIndex];
+        if (previewedCardIndices.Contains(handIndex))
+        {
+            card.CancelPreviewActions();
+            previewedCardIndices.Remove(handIndex);
+        }
+        else
+        {
+            card.PreviewActions();
+            previewedCardIndices.Add(handIndex);
+        }
+    }
+
+    /// <summary>
+    /// Play any cards currently being previewed.
+    /// </summary>
+    public void PlayPreveiwedCards()
+    {
+        foreach (int previewedCardIndex in previewedCardIndices)
+        {
+            Card card = inHandCards[previewedCardIndex];
+            card.PlayActions();
+            cardIndicesToCooldowns.Add(previewedCardIndex, card.cooldown);
+        }
+        previewedCardIndices.Clear();
+    }
+
+    /// <summary>
+    /// Fills the first empty spot in the player's hand with a card from the draw pile.
+    /// </summary>
+    /// <returns> Whether or not an empty spot was found. </returns>
     private bool DrawCard()
     {
         while (inHandCards.Count < handSize)
@@ -106,35 +169,5 @@ public class DeckManager : MonoBehaviour
             }
         }
         return false;
-    }
-
-    public void TogglePreviewCard(int handIndex)
-    {
-        if (inHandCards.Count <= handIndex || cardIndicesToCooldowns.ContainsKey(handIndex))
-        {
-            return;
-        }
-        Card card = inHandCards[handIndex];
-        if (previewedCardIndices.Contains(handIndex))
-        {
-            card.CancelPreview();
-            previewedCardIndices.Remove(handIndex);
-        }
-        else
-        {
-            card.PreviewEffect();
-            previewedCardIndices.Add(handIndex);
-        }
-    }
-
-    public void PlayPreveiwedCards()
-    {
-        foreach (int previewedCardIndex in previewedCardIndices)
-        {
-            Card card = inHandCards[previewedCardIndex];
-            card.ConfirmPreview();
-            cardIndicesToCooldowns.Add(previewedCardIndex, card.cooldown);
-        }
-        previewedCardIndices.Clear();
     }
 }
