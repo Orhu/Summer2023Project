692a7419e9d8e7177eb3d57e78456237942ae252
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-05-13 15:48:30 -0600

    Changed to BFS instead of DFS

--- Assets/ProceduralGeneration/ProceduralGeneration.cs
+++ Assets/ProceduralGeneration/ProceduralGeneration.cs
@@ -29,6 +29,21 @@ public class ProceduralGeneration : MonoBehaviour
     // The instance
     public static ProceduralGeneration proceduralGenerationInstance { get; private set; }
 
+    public class RoomGridCell
+    {
+        public Direction direction = Direction.None;
+        public bool visited = false;
+        public Vector2Int location;
+    }
+
+    List<List<RoomGridCell>> roomGrid;
+
+    public class DirectionConstraint
+    {
+        public Direction mustHave = Direction.None;
+        public Direction mustNotHave = Direction.None;
+    }
+
     /// <summary>
     /// Sets up the singleton
     /// </summary>
@@ -55,14 +70,56 @@ public class ProceduralGeneration : MonoBehaviour
     /// </summary>
     void Generate()
     {
+        // Initialize the room grid
+        roomGrid = new List<List<RoomGridCell>>();
         for (int i = 0; i < mapSize.x; i++)
         {
+            roomGrid.Add(new List<RoomGridCell>());
             for (int j = 0; j < mapSize.y; j++)
             {
-                Vector2 location;
-                location.x = i * roomSize.x * cellSize.x;
-                location.y = j * roomSize.y * cellSize.y;
-                CreateRoom(location);
+                roomGrid[i].Add(new RoomGridCell());
+                roomGrid[i][j].location = new Vector2Int(i, j);
+            }
+        }
+
+        // Start in the middle of the grid
+        Vector2Int startPos = new Vector2Int((int) mapSize.x / 2, (int) mapSize.y / 2);
+        roomGrid[(int)startPos.x][(int)startPos.y].direction = Direction.Right | Direction.Left | Direction.Up | Direction.Down;
+        roomGrid[(int)startPos.x][(int)startPos.y].visited = true;
+        Vector2 startLocation;
+        startLocation.x = (startPos.x - mapSize.x / 2) * roomSize.x * cellSize.x;
+        startLocation.y = (startPos.y - mapSize.y / 2) * roomSize.y * cellSize.y;
+        CreateRoom(startLocation, roomGrid[startPos.x][startPos.y].direction);
+
+        Queue<RoomGridCell> cellsToGenerate = new Queue<RoomGridCell>();
+        List<RoomGridCell> initialNeighbors = GetUnvisitedNeighbors(startPos);
+        
+        for (int i = 0; i < initialNeighbors.Count; i++)
+        {
+            initialNeighbors[i].visited = true;
+            cellsToGenerate.Enqueue(initialNeighbors[i]);
+        }
+
+        while (cellsToGenerate.Count != 0)
+        {
+            RoomGridCell currentCell = cellsToGenerate.Peek();
+            DirectionConstraint directionConstraint = GetDirectionConstraint(currentCell.location);
+            Direction direction = (Direction) Random.Range(0, 16);
+            direction = direction | directionConstraint.mustHave;
+            Direction invertedMustNotHave = (Direction)15 ^ directionConstraint.mustNotHave;
+            direction = direction & invertedMustNotHave;
+            Vector2 location;
+            location.x = (currentCell.location.x - mapSize.x / 2) * roomSize.x * cellSize.x;
+            location.y = (currentCell.location.y - mapSize.y / 2) * roomSize.y * cellSize.y;
+            CreateRoom(location, direction);
+            currentCell.direction = direction;
+
+            cellsToGenerate.Dequeue();
+            List<RoomGridCell> newNeighbors = GetUnvisitedNeighbors(currentCell.location, direction);
+            for (int i = 0; i < newNeighbors.Count; i++)
+            {
+                newNeighbors[i].visited = true;
+                cellsToGenerate.Enqueue(newNeighbors[i]);
             }
         }
     }
@@ -71,14 +128,15 @@ public class ProceduralGeneration : MonoBehaviour
     /// Creates a room with the given tilemap and a given location
     /// </summary>
     /// <param name="location"> The location of the new room </param>
-    void CreateRoom(Vector2 location)
+    /// <param name="direction"> The direction of the new room </param>
+    void CreateRoom(Vector2 location, Direction direction)
     {
         GameObject newRoom = Instantiate(room, location, Quaternion.identity);
         newRoom.transform.parent = this.transform;
         newRoom.GetComponent<Room>().tile = room.GetComponent<Room>().tile;
         newRoom.GetComponent<Room>().size = roomSize;
         newRoom.GetComponent<Room>().cellSize = cellSize;
-        newRoom.GetComponent<Room>().directions = Direction.Right | Direction.Up | Direction.Left | Direction.Down;
+        newRoom.GetComponent<Room>().directions = direction;
         newRoom.SetActive(true);
     }
 
@@ -98,6 +156,114 @@ public class ProceduralGeneration : MonoBehaviour
     public void AddRoomGenerationParameters(RoomGenerationParameters addedRoomParams)
     {
         roomGenerationParameters.Add(addedRoomParams);
+    }
+
+    List<RoomGridCell> GetUnvisitedNeighbors(Vector2Int pos, Direction direction = (Direction) 15)
+    {
+        List<RoomGridCell> neighbors = new List<RoomGridCell>();
+
+        if ((direction & Direction.Right) != Direction.None && pos.x < mapSize.x - 1 && !roomGrid[pos.x + 1][pos.y].visited)
+        {
+            neighbors.Add(roomGrid[pos.x + 1][pos.y]);
+        }
+        if ((direction & Direction.Up) != Direction.None && pos.y < mapSize.y - 1 && !roomGrid[pos.x][pos.y + 1].visited)
+        {
+            neighbors.Add(roomGrid[pos.x][pos.y + 1]);
+        }
+        if ((direction & Direction.Left) != Direction.None && pos.x > 0 && !roomGrid[pos.x - 1][pos.y].visited)
+        {
+            neighbors.Add(roomGrid[pos.x - 1][pos.y]);
+        }
+        if ((direction & Direction.Down) != Direction.None && pos.y > 0 && !roomGrid[pos.x][pos.y - 1].visited)
+        {
+            neighbors.Add(roomGrid[pos.x][pos.y - 1]);
+        }
+
+        return neighbors;
+    }
+
+    DirectionConstraint GetDirectionConstraint(Vector2Int pos)
+    {
+        DirectionConstraint directionConstraint = new DirectionConstraint();
+        if (pos.x == mapSize.x - 1)
+        {
+            directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Right;
+        }
+        if (pos.y == mapSize.y - 1)
+        {
+            directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Up;
+        }
+        if (pos.x == 0)
+        {
+            directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Left;
+        }
+        if (pos.y == 0)
+        {
+            directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Down;
+        }
+
+        if (pos.x < mapSize.x - 1)
+        {
+            RoomGridCell rightNeighbor = roomGrid[pos.x + 1][pos.y];
+            if (rightNeighbor.visited)
+            {
+                if ((rightNeighbor.direction & Direction.Left) != Direction.None)
+                {
+                    directionConstraint.mustHave = directionConstraint.mustHave | Direction.Right;
+                }
+                else
+                {
+                    directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Right;
+                }
+            }
+        }
+        if (pos.y < mapSize.y - 1)
+        {
+            RoomGridCell upNeighbor = roomGrid[pos.x][pos.y + 1];
+            if (upNeighbor.visited)
+            {
+                if ((upNeighbor.direction & Direction.Down) != Direction.None)
+                {
+                    directionConstraint.mustHave = directionConstraint.mustHave | Direction.Up;
+                }
+                else
+                {
+                    directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Up;
+                }
+            }
+        }
+        if (pos.x > 0)
+        {
+            RoomGridCell leftNeighbor = roomGrid[pos.x - 1][pos.y];
+            if (leftNeighbor.visited)
+            {
+                if ((leftNeighbor.direction & Direction.Right) != Direction.None)
+                {
+                    directionConstraint.mustHave = directionConstraint.mustHave | Direction.Left;
+                }
+                else
+                {
+                    directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Left;
+                }
+            }
+        }
+        if (pos.y > 0)
+        {
+            RoomGridCell downNeighbor = roomGrid[pos.x][pos.y - 1];
+            if (downNeighbor.visited)
+            {
+                if ((downNeighbor.direction & Direction.Up) != Direction.None)
+                {
+                    directionConstraint.mustHave = directionConstraint.mustHave | Direction.Down;
+                }
+                else
+                {
+                    directionConstraint.mustNotHave = directionConstraint.mustNotHave | Direction.Down;
+                }
+            }
+        }
+
+        return directionConstraint;
     }
 }
 
