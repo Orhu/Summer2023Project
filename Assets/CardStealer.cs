e25448bc5120dbc16c65ae3c569030c6014a3972
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-05-16 12:58:57 -0600

    Chests

--- Assets/CardStealer.cs
+++ Assets/CardStealer.cs
@@ -4,17 +4,9 @@ using UnityEngine;
 
 public class CardStealer : MonoBehaviour
 {
-    private GameObject player;
-
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-    }
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == player && DeckManager.playerDeck.cards.Count > 0)
+        if (collision.CompareTag("Player") && DeckManager.playerDeck.cards.Count > 0)
         {
             DeckManager.playerDeck.RemoveCard(DeckManager.playerDeck.cards[Random.Range(0, DeckManager.playerDeck.cards.Count - 1)]);
         }
