c175e8cab7aae11a8dd5992f9b982ed4a4e523e8
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-05-22 14:41:22 -0600

    I found a package and modified it a bit

---
+++ Assets/Editor/EditorStubs.cs
@@ -0,0 +1,4 @@
+//This is a generated file - do not modify.
+[UnityEditor.CustomEditor(typeof(CardSystem.Effects.SpawnProjectile))]
+public partial class SpawnProjectileEditorStub : UnityEditor.Editor { }
+
