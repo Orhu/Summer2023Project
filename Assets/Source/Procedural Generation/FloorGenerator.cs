a035e7d315e16290d91e5cc3532dc3a161768e07
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-05-29 21:56:00 -0600

    Camera now correctly follows the rooms
    And scrolls in the boss room (still jank lmao)

--- Assets/Source/Procedural Generation/FloorGenerator.cs
+++ Assets/Source/Procedural Generation/FloorGenerator.cs
@@ -14,6 +14,26 @@ public class FloorGenerator : MonoBehaviour
     // A reference to the generated map
     [HideInInspector] public Map map;
 
+    // The room the player is currently in
+    [HideInInspector] public Room currentRoom;
+
+    // The instance
+    public static FloorGenerator floorGeneratorInstance { get; private set; }
+
+    /// <summary>
+    /// Sets up the singleton
+    /// </summary>
+    void Awake()
+    {
+        if (floorGeneratorInstance != null && floorGeneratorInstance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        floorGeneratorInstance = this;
+    }
+
+
     /// <summary>
     /// Generates the layout and exteriors of the room
     /// </summary>
@@ -21,5 +41,17 @@ public class FloorGenerator : MonoBehaviour
     {
         map = GetComponent<LayoutGenerator>().Generate(floorGenerationParameters.layoutGenerationParameters);
         GetComponent<RoomExteriorGenerator>().Generate(floorGenerationParameters.roomTypesToExteriorGenerationParameters, map, floorGenerationParameters.roomSize);
+    }
+
+    /// <summary>
+    /// Transforms a map location to a world location
+    /// </summary>
+    /// <param name="mapLocation"> The location to transform </param>
+    /// <param name="startLocation"> The start location (aka midpoint of the map) </param>
+    /// <param name="roomSize"> The room size </param>
+    /// <returns> The world location </returns>
+    static public Vector2 TransformMapToWorld(Vector2Int mapLocation, Vector2Int startLocation, Vector2Int roomSize)
+    {
+        return new Vector2((mapLocation.x - startLocation.x) * roomSize.x, (mapLocation.y - startLocation.y) * roomSize.y);
     }
 }
