c7bf23cbb48bb9fbfb63b65b0d9a891752a266d6
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-06-03 16:54:06 -0600

    I was a dumb

--- Assets/Source/Health & Status Effects/StatusEffect.cs
+++ Assets/Source/Health & Status Effects/StatusEffect.cs
@@ -42,6 +42,7 @@ public abstract class StatusEffect : ScriptableObject
     {
         StatusEffect instance = Instantiate(this);
 
+        instance.remainingDuration = duration;
         instance.gameObject = gameObject;
 
         if (visualEffect != null)
