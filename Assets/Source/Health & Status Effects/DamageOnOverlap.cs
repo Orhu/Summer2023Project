612174fc72983126b9e7ea27cf7609f035f310e5
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-06-05 15:28:48 -0600

    Fix me accidently deleting my work

--- Assets/Source/Health & Status Effects/DamageOnOverlap.cs
+++ Assets/Source/Health & Status Effects/DamageOnOverlap.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 /// <summary>
@@ -9,8 +10,41 @@ public class DamageOnOverlap : MonoBehaviour
     [Tooltip("The damage that will be dealt.")]
     [SerializeField] private DamageData damageData;
 
+    [Tooltip("The frequency at which damage will be dealt.")]
+    [SerializeField] private float damageInterval = 0.5f;
+
+    [Tooltip("Whether or not this should wait for the damage interval to deal damage when first overlapping.")]
+    [SerializeField] private bool immediatelyDamage = true;
+
+    // The collider used for overlap detection.
+    private new Collider2D collider;
+
+    private void Awake()
+    {
+        collider = GetComponent<Collider2D>();
+    }
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        other.GetComponent<Health>()?.ReceiveAttack(damageData);
+        Health health = other.GetComponent<Health>();
+        if (health == null) { return; }
+
+        StartCoroutine(DealDamage(other, health));
+    }
+
+    private IEnumerator DealDamage(Collider2D other, Health health)
+    {
+        if (immediatelyDamage)
+        {
+            health.ReceiveAttack(damageData);
+        }
+
+        yield return new WaitForSeconds(damageInterval);
+        while (collider.IsTouching(other))
+        {
+            health.ReceiveAttack(damageData);
+            yield return new WaitForSeconds(damageInterval);
+        }
     }
 }
