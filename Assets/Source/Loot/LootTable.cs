bc36702a1c18a9771255f6cd02315fa6ddd8b92f
Hero <humphriesa02@gmail.com>
2023-06-05 23:07:39 -0700

    Attempt to prevent duplicates completely

--- Assets/Source/Loot/LootTable.cs
+++ Assets/Source/Loot/LootTable.cs
@@ -14,6 +14,14 @@ public struct WeightedLootItems<T>
     public T lootItem;
     // The RNG weight of that item
     public float weight;
+
+    public WeightedLootItems<T> Clone(T lootItem, float weight)
+    {
+        var clone = new WeightedLootItems<T>();
+        clone.lootItem = lootItem;
+        clone.weight = weight;
+        return clone;
+    }
 }
 
 /// <summary>
@@ -51,10 +59,40 @@ public class LootTable<T> : ScriptableObject
         else
         {
             throw new System.Exception("Table does not have any items in it.");
-        }
-        
+        } 
     }
 
+    /// <summary>
+    /// Using a copy of the weightedLootList, randomly select cards to return and delete
+    /// </summary>
+    /// <param name="copyOfWeightedLoot">A copy of the weighted loot list</param>
+    /// <returns>A random card</returns>
+    public T PullAndRemoveFromTable(List<WeightedLootItems<T>> copyOfWeightedLoot)
+    {
+        float totalWeight = 0;
+        T selected = default(T);
+        int index = 0;
+        if (copyOfWeightedLoot.Count > 0)
+        {
+            for(int i = 0; i < copyOfWeightedLoot.Count; i++)
+            {
+                float weight = copyOfWeightedLoot[i].weight;
+                float rand = Random.Range(0, totalWeight + weight);
+                if (rand >= totalWeight)
+                {
+                    selected = copyOfWeightedLoot[i].lootItem;
+                    index = i;
+                }
+                totalWeight += weight;
+            }
+            copyOfWeightedLoot.RemoveAt(index);
+            return selected;
+        }
+        else
+        {
+            return default(T);
+        }
+    }
     /// <summary>
     /// Pulls multiple items randomly from the table based on weight
     /// </summary>
@@ -62,20 +100,48 @@ public class LootTable<T> : ScriptableObject
     /// <returns>List of items pulled from the table</returns>
     public List<T> PullMultipleFromTable(int pullCount)
     {
-        List<T> itemList = new List<T>();
-        for (int i = 0; i < pullCount; i++)
+        if (pullCount <= weightedLoot.Count)
         {
-            T item = PullFromTable();
-            // Amount of attempts to pull a non-duplicate card
-            int pullAttempts = 0;
-            while (itemList.Contains(item) || pullAttempts < 5) // Give up after 5 attempts (table does not have enough distinct cards?)
+            List<T> itemList = new List<T>();
+            List<WeightedLootItems<T>> backup = weightedLoot.ConvertAll(weightedLootItem => weightedLootItem.Clone(weightedLootItem.lootItem, weightedLootItem.weight));
+            for (int i = 0; i < pullCount; i++)
             {
-                item = PullFromTable();
-                pullAttempts++;
+                // Pull and remove an item from the list of items
+                T item = PullAndRemoveFromTable(backup);
+                // If it's already in the list we will be returning,
+                // keep pulling and deleting from the list until we encounter a non dupe
+                while (itemList.Contains(item) && backup.Count > 0)
+                {
+                    item = PullAndRemoveFromTable(backup);
+                }
+
+                // We find a non dupe
+                if(item is { })
+                {
+                    itemList.Add(item);
+                }
+                // The list has run out
+                else
+                {
+                    // If we have the same size list as what we were looking for,
+                    // we're fine
+                    if (itemList.Count == pullCount)
+                    {
+                        return itemList;
+                    }
+                    // If not, we have an imbalance
+                    else
+                    {
+                        throw new System.Exception("Too many duplicates for pullCount");
+                    }     
+                }
+                
             }
-            // Item we pulled is distinct (hopefully), add it to the list
-            itemList.Add(item);
+            return itemList;
         }
-        return itemList;
+        else
+        {
+            throw new System.Exception("Number of items pulled exceeds table size");
+        }
     }
 }
