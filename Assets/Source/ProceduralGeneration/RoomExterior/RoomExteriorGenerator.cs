87381eb86674eccbef2f9d0a0628af935f5bb7cb
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-06-06 10:03:46 -0600

    Made floor sorting order -100

--- Assets/Source/ProceduralGeneration/RoomExterior/RoomExteriorGenerator.cs
+++ Assets/Source/ProceduralGeneration/RoomExterior/RoomExteriorGenerator.cs
@@ -307,7 +307,7 @@ public class RoomExteriorGenerator : MonoBehaviour
         floorContainer.transform.parent = room.transform;
         floorContainer.transform.localPosition = new Vector3(0, 0, 0);
         floorContainer.AddComponent<SpriteRenderer>().sprite = exteriorGenerationParameters.floorSprites[Random.Range(0, exteriorGenerationParameters.floorSprites.Count)];
-        floorContainer.GetComponent<SpriteRenderer>().sortingOrder = -1;
+        floorContainer.GetComponent<SpriteRenderer>().sortingOrder = -100;
         floorContainer.SetActive(false);
     }
     /// <summary>
