6b819c28bd90bb5f0b0d0c1d08c0535e17526d19
Orhu <mcdonoughmalcolm@gmail.com>
2023-06-15 11:37:11 -0500

    stupid comment updates :P

--- Assets/Content/Shaders/EchoShader/EchoManager.cs
+++ Assets/Content/Shaders/EchoShader/EchoManager.cs
@@ -2,32 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///  Script for Echoing Shout's shader. TOUCH THIS AND YOU DIE!!!!!!!!!!
+///  Also I'm not serializing this because it shouldn't be serialized :)
+/// </summary>
 public class EchoManager : MonoBehaviour
 {
     // smack this bad boy on the gameobject you instantiate when casting echoing shout as a child of the player
 
     [Tooltip("Length of echo shader animation")]
-    [SerializeField] private float _echoTime = 0.5f; // Length of echo
+    // length of echo in seconds
+    [SerializeField] private float _echoTime = 3.5f; // should be constant and roughly equal to the lifetime of echoing shout so that the ripple moves with the bullets (21 tiles / 6 tiles/s = 3.5s)
+    
+    // reference to coroutine that controls the echo effect
+    private Coroutine _echoCoroutine;
 
-    private Coroutine _echoCoroutine; // reference to coroutine that controls the echo effect
+    // material on game object
+    private Material _material;
 
-    private Material _material; // material on game object
+    // shader property conversion (unnecessary comment tbh)
+    private static int _waveDistanceFromCenter = Shader.PropertyToID("_WaveDistanceFromCenter"); 
 
-    private static int _waveDistanceFromCenter = Shader.PropertyToID("_WaveDistanceFromCenter"); // shader property (unnecessary comment tbh)
-
+    /// <summary>
+    /// triggers the echo
+    /// </summary>
     private void Awake()
     {
         _material = GetComponent<SpriteRenderer>().material;
         CallEcho();
     }
 
-    // triggers the echo effect
+    /// <summary>
+    /// triggers echo coroutine
+    /// </summary>
     public void CallEcho()
     {
         _echoCoroutine = StartCoroutine(EchoAction(-0.1f, 1f));
     }
 
-    // echo effect controlling subroutine
+    /// <summary>
+    /// echo effect controlling subroutine
+    /// </summary>
+    /// <param name="startPos"> start val (ALWAYS -0.1f)</param>
+    /// <param name="endPos"> end val (ALWAYS 1f)</param>
+    /// <returns>cool effect idk</returns>    
     private IEnumerator EchoAction(float startPos, float endPos)
     {
         _material.SetFloat(_waveDistanceFromCenter, startPos);
@@ -40,7 +58,7 @@ public class EchoManager : MonoBehaviour
             elapsedTime += Time.deltaTime;
 
             lerpedAmount = Mathf.Lerp(startPos, endPos, (elapsedTime / _echoTime));
-            _material.SetFloat(_waveDistanceFromCenter, lerpedAmount);
+            _material.SetFloat(_waveDistanceFromCenter, lerpedAmount); // use lerp amount to set progress of ripple effect
 
             yield return null;
         }
