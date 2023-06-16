a1a7cc19417c49a0c28e51ab7939ce4d421f60e2
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-06-16 12:21:55 -0600

    Added tooltips
    Frick how am I going to do this

--- Assets/Source/ProceduralGeneration/RoomType.cs
+++ Assets/Source/ProceduralGeneration/RoomType.cs
@@ -7,10 +7,21 @@ using UnityEngine;
 /// </summary>
 public class RoomType : ScriptableObject
 {
+    [Tooltip("Whether or not this room type can only spawn as a dead end")]
     public bool deadEnd { get; private set; }
+
+    [Tooltip("Whether or not this room type determines it's room size explicitly (only allowed for rooms that are dead ends)")]
     public bool useExplicitRoomSize { get; private set; }
+
+    [Tooltip("Whether or not this room is a normal room (this might not be needed)")]
     public bool normalRoom { get; private set; }
+
+    [Tooltip("The size multiplier of this room: What size is this room compared to the size of a normal room?")]
     public Vector2Int sizeMultiplier { get; private set; }
+
+    [Tooltip("The explicit room size of this room type")]
     public Vector2Int explicitRoomSize { get; private set; }
+
+    [Tooltip("The acceptable cells doors can appear in")]
     public List<Vector2Int> acceptableDoorCells { get; private set; }
 }
