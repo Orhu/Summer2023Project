727c1830114873229ced176f11d4b105ef78de26
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-06-17 17:01:47 -0600

    Comments

--- Assets/Source/Player/ChannelAbility.cs
+++ Assets/Source/Player/ChannelAbility.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace Cardificer
 {
+    /// <summary>
+    /// Component that allows for something to channel in order to reset its cooldowns.
+    /// </summary>
     public class ChannelAbility : MonoBehaviour
     {
         [Tooltip("The time it takes to channel in seconds")]
@@ -14,22 +17,34 @@ namespace Cardificer
         [Tooltip("The interval at which the status effect is applied")]
         [SerializeField] private float statusEffectInertval = 0.1f;
 
+        /// <summary>
+        /// Starts channeling and applying the status effect.
+        /// </summary>
         public void StartChanneling()
         {
             Invoke("Channel", channelDuration);
             InvokeRepeating("ApplyStatusEffect", 0f, statusEffectInertval);
         }
 
+        /// <summary>
+        /// Cancels the channeling without causing its final effect.
+        /// </summary>
         public void StopChanneling()
         {
             CancelInvoke();
         }
 
+        /// <summary>
+        /// Applies the status effect of this channeling ability.
+        /// </summary>
         private void ApplyStatusEffect()
         {
             Player.health.ReceiveAttack(new DamageData(new List<StatusEffect>() { statusEffect }, null));
         }
 
+        /// <summary>
+        /// Resets the player's cooldowns, and stops applying status effects.
+        /// </summary>
         private void Channel()
         {
             Deck.playerDeck.ClearCooldowns();
