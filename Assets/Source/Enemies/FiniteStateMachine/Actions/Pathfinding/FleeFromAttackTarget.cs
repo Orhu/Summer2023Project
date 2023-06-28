a88c59c86743f1a633ab6b705ade4d02d78ff14d
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-06-28 13:29:01 -0600

    Fix flee from target

--- Assets/Source/Enemies/FiniteStateMachine/Actions/Pathfinding/FleeFromAttackTarget.cs
+++ Assets/Source/Enemies/FiniteStateMachine/Actions/Pathfinding/FleeFromAttackTarget.cs
@@ -6,16 +6,16 @@ using UnityEngine;
 namespace Cardificer.FiniteStateMachine
 {
     /// <summary>
-    /// Represents an action that path finds away from the current target. Will override the pathfinding target.
+    /// Represents an action that sets the pathfinding target away from the current attack target.
     /// </summary>
     [CreateAssetMenu(menuName="FSM/Actions/Pathfinding/Flee from Attack Target")]
-    public class FleeFromAttackTarget : ChaseTarget
+    public class FleeFromAttackTarget : SingleAction
     {
         [Tooltip("The maximum number of tiles to flee at a time. Larger number means its better at getting out of dead ends.")] [Range(1,5)]
         [SerializeField] private int fleeTiles = 1;
 
         /// <summary>
-        /// Moves directly towards the pathfinding target.
+        /// Sets the pathfinding target to the furthest tile within flee tiles of the state machine.
         /// </summary>
         /// <param name="stateMachine"> StateMachine to be used </param>
         /// <returns> Updates move input every frame until duration has elapsed. </returns>
@@ -44,7 +44,8 @@ namespace Cardificer.FiniteStateMachine
                 }
             }
 
-            return base.PlayAction(stateMachine);
+            stateMachine.cooldownData.cooldownReady[this] = true;
+            yield break;
         }
 
         /// <summary>
