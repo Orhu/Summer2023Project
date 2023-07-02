3427624fce374a2c4ceaa84518ee459fdb3c89a5
Alex <humphriesa02@gmail.com>
2023-07-01 17:06:39 -0700

    Beginning card list implementation

--- Assets/Source/UI/Menu/CardMenu.cs
+++ Assets/Source/UI/Menu/CardMenu.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Cardificer
 {
@@ -8,16 +9,44 @@ namespace Cardificer
     /// </summary>
     public class CardMenu : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
+        [Tooltip("List of cardRender UI elements for displaying booster pack cards")]
+        public List<CardRenderer> cardRenderers = new List<CardRenderer>();
 
-        }
+        // UI area for adding cardRenderer UI objects
+        [SerializeField] private GameObject scrollCardLayoutArea;
+
+        // The card renderer prefab to instantiate.
+        public CardRenderer cardRendererTemplate;
 
         // Update is called once per frame
         void Update()
         {
+            if(scrollCardLayoutArea.transform.childCount != Deck.playerDeck.cards.Count)
+            {
+                ResetScrollCardLayoutArea();
+                InstantiateCardLayoutArea();
+            }
+        }
 
+        void ResetScrollCardLayoutArea()
+        {
+            foreach (Transform child in scrollCardLayoutArea.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+
+        void InstantiateCardLayoutArea()
+        {
+            for (int i = 0; i < Deck.playerDeck.cards.Count; i++)
+            {
+                // Instantiate the cardRenderer Game Object in the cardLayout area
+                GameObject tempCardRendererGameObject = Instantiate(cardRendererTemplate.gameObject, scrollCardLayoutArea.transform);
+                // Assign the game object a card.
+                tempCardRendererGameObject.GetComponent<CardRenderer>().card = Deck.playerDeck.cards[i];
+                // Adds the cardRenderer to the list of cardRenderers
+                cardRenderers.Add(tempCardRendererGameObject.GetComponent<CardRenderer>());
+            }
         }
     }
 }
