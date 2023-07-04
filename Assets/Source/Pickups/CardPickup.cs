822d76a460a680f8cfcf06ab4dfe968baaa09d0f
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-07-04 16:16:26 -0600

    max counts

--- Assets/Source/Pickups/CardPickup.cs
+++ Assets/Source/Pickups/CardPickup.cs
@@ -20,6 +20,7 @@ namespace Cardificer
         /// </summary>
         private void Start()
         {
+            lootTable = Instantiate(lootTable);
             card = lootTable.weightedLoot.GetRandomThing(transform.position);
             GetComponentInChildren<CardRenderer>(true).card = card;
             GetComponentInChildren<SpriteRenderer>().sprite = card.runeImage;
