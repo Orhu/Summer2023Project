a458b6eee52c6aea45d868846e552dcb989f0b9c
Alex <humphriesa02@gmail.com>
2023-07-03 20:59:10 -0700

    Card Renderers and Card List changes
    Still have some broken issues

--- Assets/Source/UI/Card Renderer/CardRenderer.cs
+++ Assets/Source/UI/Card Renderer/CardRenderer.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
 
 namespace Cardificer
 {
     /// <summary>
     /// A component for rendering cards in UI space.
     /// </summary>
-    public class CardRenderer : MonoBehaviour
+    public class CardRenderer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler // Handlers function as UI OnMouseEnter
     {
         [Tooltip("The card to render.")]
         [SerializeField] private Card _card;
@@ -17,6 +19,22 @@ namespace Cardificer
 
         [Tooltip("Reference to the card's compendium button")]
         [SerializeField] private Button compendiumButton;
+
+        // To remember the original scale of the card
+        [SerializeField]private Vector3 originalScale;
+
+        [Tooltip("Scaling factor of the scaling animation")]
+        public float scaleFactor = 1.5f;
+
+        [Tooltip("Duration of the scaling animation")]
+        public float scaleDuration = 0.25f;
+
+        [Tooltip("Set by the CardRenderer's manager when the card is selected.")]
+        public bool isSelected;
+
+        // Simple flag for knowing when the mouse is hovering over
+        // the CardRenderer
+        private bool isHovered;
 
         public Card card
         {
@@ -28,9 +46,14 @@ namespace Cardificer
                 links.nameTextBox.enabled = shouldEnable;
                 links.descriptionTextBox.enabled = shouldEnable;
                 links.backgroundSprite.enabled = shouldEnable;
+                links.outlineSprite.enabled = shouldEnable;
+                links.chordEffectText.enabled = shouldEnable;
+                links.rarityImage.enabled = shouldEnable;
+                links.damageContainer.SetActive(shouldEnable);
                 links.cardSprite.enabled = shouldEnable;
                 links.projectileTypeImage.enabled = shouldEnable;
                 links.flavorTextBox.enabled = shouldEnable;
+                links.coolDownOverlayText.enabled = shouldEnable;
                 if (shouldEnable)
                 {
                     // Name of the card
@@ -105,6 +128,14 @@ namespace Cardificer
         }
 
         /// <summary>
+        /// Assign member variable values.
+        /// </summary>
+        private void Start()
+        {
+            originalScale = transform.localScale;
+        }
+
+        /// <summary>
         /// Refreshes the rendering on property change.
         /// </summary>
         public void Update()
@@ -118,23 +149,114 @@ namespace Cardificer
         public void OnToggle()
         {
             // See if the toggle is on or off
-            // (this function is called before this value is set)
             bool isToggled = GetComponent<Toggle>().isOn;
-            if (!isToggled) // about to turn the toggle on
+
+            if (isToggled) // We've toggled the card
             {
-                // Make the card grow
-                GetComponent<Animator>().Play("A_CardRenderer_Enlarge");
-                // Turn on the compendium button
-                compendiumButton.gameObject.SetActive(false);
+                // turn on the outline
+                links.outlineSprite.gameObject.SetActive(true);
             }
-            else // about to turn the toggle off
+            else // deactivated toggle
             {
-                // Shrink the card
-                GetComponent<Animator>().Play("A_CardRenderer_Shrink");
-                // turn the compendium button off
-                compendiumButton.gameObject.SetActive(true);
+                // Turn off the outline
+                links.outlineSprite.gameObject.SetActive(false);
+                // Shrink the card if it's being hovered
+                if (isHovered)
+                {
+                    StartCoroutine(ScaleCardRenderer(false));
+                    isHovered = false;
+                }
             }
         }
+
+        /// <summary>
+        /// Called when the mouse hovers over the card.
+        /// </summary>
+        /// <param name="eventData">Not used</param>
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (!isHovered)
+            {
+                // Make the card grow
+                StartCoroutine(ScaleCardRenderer(true));
+                // Turn on the compendium button
+                compendiumButton.gameObject.SetActive(true);
+                // Set hovered to true
+                isHovered = true;
+            }
+        }
+
+        /// <summary>
+        /// Called when the mouse stops hovering over the card
+        /// </summary>
+        /// <param name="eventData">Not used</param>
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            if (isHovered)
+            {
+                // Shrink the card
+                StartCoroutine(ScaleCardRenderer(false));
+                // Turn off the compendium button
+                compendiumButton.gameObject.SetActive(false);
+                // Set hovered to false
+                isHovered = false;
+            }
+        }
+
+        /// <summary>
+        /// When cards 
+        /// </summary>
+        private void OnDisable()
+        {
+            GetComponent<Toggle>().isOn = false;
+            if (isHovered)
+            {
+                transform.localScale = originalScale;
+            }
+            
+        }
+
+        /// <summary>
+        /// Small function that dynamically animates
+        /// the scaling of the CardRenderer up or down by two
+        /// </summary>
+        /// <param name="scaleUp">Whether we are scaling up or down</param>
+        private IEnumerator ScaleCardRenderer(bool scaleUp)
+        {
+            float timePassed = 0f;
+            Vector3 targetScale = originalScale * scaleFactor;
+            // Scaling up
+            if (scaleUp)
+            {
+                while (timePassed < scaleDuration)
+                {
+                    transform.localScale = Vector3.Lerp(originalScale, targetScale, timePassed / scaleDuration);
+                    timePassed += Time.unscaledDeltaTime;
+                    yield return null;
+                }
+
+                transform.localScale = targetScale;
+            }
+            // Scaling down
+            else
+            {
+                while (timePassed < scaleDuration)
+                {
+                    transform.localScale = Vector3.Lerp(targetScale, originalScale, timePassed / scaleDuration);
+                    timePassed += Time.unscaledDeltaTime;
+                    yield return null;
+                }
+
+                transform.localScale = originalScale;
+            }
+        }
+
+        public void SetScale(Vector3 theScale)
+        {
+            transform.localScale = theScale;
+            originalScale = theScale;
+        }
+
 
         /// <summary>
         /// Used to flip rendered card from effect to action and back
@@ -161,6 +283,9 @@ namespace Cardificer
 
             [Tooltip("The image used to render the background of the card.")]
             public Image backgroundSprite;
+
+            [Tooltip("The image used to show when a card is selected by outlining it.")]
+            public Image outlineSprite;
 
             [Tooltip("The image used to render the card specific sprite.")]
             public Image cardSprite;
