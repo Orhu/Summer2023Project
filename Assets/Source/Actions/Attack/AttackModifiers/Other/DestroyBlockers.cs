20995346af3930039d5e085fca458f7b5cb5c4ee
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-07-08 16:07:48 -0600

    Destroyable blockers probly working
    too lazy to test because compoents new components arent added to all the old levels

---
+++ Assets/Source/Actions/Attack/AttackModifiers/Other/DestroyBlockers.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+
+namespace Cardificer
+{
+    /// <summary>
+    /// Causes an attack to destroy blockers.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewDestroyBlockers", menuName = "Cards/AttackModifers/Destroy Blockers")]
+    public class DestroyBlockers : AttackModifier
+    {
+        // Binds Destroys blockers on hit
+        public override Projectile modifiedProjectile
+        {
+            set
+            {
+                value.onHit +=
+                    (Collision2D collision) =>
+                    {
+                        if (collision.gameObject.layer != LayerMask.NameToLayer("Blockers")) { return; }
+
+                        if (--value.remainingHits > 0)
+                        {
+                            value.CancelInvoke("DestroyOnWallHit");
+                        }
+                        Destroy(collision.gameObject);
+                    };
+            }
+        }
+    }
+}
