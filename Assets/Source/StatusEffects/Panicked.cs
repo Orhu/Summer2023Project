6abb730425db8413bd60ecf80e9229dd5cf7002e
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-07-10 16:25:37 -0600

    paniced

---
+++ Assets/Source/StatusEffects/Panicked.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+namespace Cardificer
+{
+    /// <summary>
+    /// A status effect that prevents movement entirely.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewPanicked", menuName = "Status Effects/Panicked")]
+    public class Panicked : StatusEffect
+    {
+        /// <summary>
+        /// Creates a new status effect that is a copy of the caller.
+        /// </summary>
+        /// <param name="gameObject"> The object to apply the status effect.</param>
+        /// <returns> The status effect that was created. </returns>
+        public override StatusEffect CreateCopy(GameObject gameObject)
+        {
+            Panicked instance = (Panicked)base.CreateCopy(gameObject);
+
+            gameObject.GetComponent<Movement>().requestSpeedModifications += instance.ReverseMovement;
+
+            return instance;
+        }
+
+
+        /// <summary>
+        /// Stacks this effect onto another status effect.
+        /// </summary>
+        /// <param name="other"> The other particle effect to stack this onto. </param>
+        /// <returns> Whether or not this status effect was consumed by the stacking. </returns>
+        public override bool Stack(StatusEffect other)
+        {
+            if (!base.Stack(other))
+            {
+                return false;
+            }
+
+            other.remainingDuration = Mathf.Max(duration, other.remainingDuration);
+            return true;
+        }
+
+        /// <summary>
+        /// Responds to a movement components speed modification request, and sets the speed to 0.
+        /// </summary>
+        /// <param name="speed"> The speed variable to be modified. </param>
+        private void ReverseMovement(ref float speed)
+        {
+            speed *= -1;
+        }
+
+        /// <summary>
+        /// Cleans up binding.
+        /// </summary>
+        private new void OnDestroy()
+        {
+            if (gameObject != null)
+            {
+                gameObject.GetComponent<Movement>().requestSpeedModifications -= ReverseMovement;
+            }
+            base.OnDestroy();
+        }
+    }
+}
