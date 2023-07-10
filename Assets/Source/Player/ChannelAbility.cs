8e3f6de2624b5a4a92c2c764161131d8ad01792b
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-07-10 17:02:49 -0600

    Update ChannelAbility.cs

--- Assets/Source/Player/ChannelAbility.cs
+++ Assets/Source/Player/ChannelAbility.cs
@@ -8,8 +8,14 @@ namespace Cardificer
     /// </summary>
     public class ChannelAbility : MonoBehaviour
     {
-        [Tooltip("The time it takes to channel in seconds")]
-        [SerializeField] private float channelDuration = 2f;
+        [Tooltip("The cooldown multiplier when channeling is first started.")] [Min(1f)]
+        [SerializeField] private float initialCooldownReduction = 1f;
+
+        [Tooltip("The max cooldown reduction from channeling.")] [Min(1f)]
+        [SerializeField] private float maxCooldownReduction = 3f;
+
+        [Tooltip("The amount of additional cooldown reduction gained every second")]
+        [SerializeField] private float cooldownReductionIncreaseRate = 1f;
 
         [Tooltip("The status effect to apply while channeling")]
         [SerializeField] private StatusEffect statusEffect;
@@ -17,12 +23,20 @@ namespace Cardificer
         [Tooltip("The interval at which the status effect is applied")]
         [SerializeField] private float statusEffectInertval = 0.1f;
 
+
+        // Whether or not this is currently channeling
+        public bool isChanneling { get; private set; } = false;
+
+        private float currentCooldownReduction;
+
         /// <summary>
         /// Starts channeling and applying the status effect.
         /// </summary>
         public void StartChanneling()
         {
-            Invoke("Channel", channelDuration);
+            isChanneling = true;
+            currentCooldownReduction = initialCooldownReduction;
+            Deck.playerDeck.cooldownReduction *= initialCooldownReduction;
             InvokeRepeating("ApplyStatusEffect", 0f, statusEffectInertval);
         }
 
@@ -31,6 +45,7 @@ namespace Cardificer
         /// </summary>
         public void StopChanneling()
         {
+            isChanneling = false;
             CancelInvoke();
         }
 
@@ -49,6 +64,17 @@ namespace Cardificer
         {
             Deck.playerDeck.ClearCooldowns();
             CancelInvoke();
+        }
+
+        /// <summary>
+        /// Updates the player's cooldowns
+        /// </summary>
+        private void Update()
+        {
+            if (!isChanneling) { return; }
+            Deck.playerDeck.cooldownReduction /= currentCooldownReduction;
+            currentCooldownReduction += cooldownReductionIncreaseRate * Time.deltaTime;
+            Deck.playerDeck.cooldownReduction *= currentCooldownReduction;
         }
     }
 }
