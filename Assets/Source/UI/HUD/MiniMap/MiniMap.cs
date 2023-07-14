334e5462f74e7bb48aa61b450e90ee753cb5dbc7
Hero <humphriesa02@gmail.com>
2023-07-13 21:41:06 -0700

    Minimap kinda working

--- Assets/Source/UI/HUD/MiniMap/MiniMap.cs
+++ Assets/Source/UI/HUD/MiniMap/MiniMap.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 namespace Cardificer{
     public class MiniMap : MonoBehaviour
     {
         [Tooltip("Grid layout for organizing room visuals")]
-        [SerializeField] private GridLayoutGroup roomImageContainer;
+        [SerializeField] private GameObject roomImageContainer;
 
         [Tooltip("Room visual prefab, change it's sprite to change the background color of the room visual.")]
         [SerializeField] private GameObject roomVisualPrefab;
@@ -24,15 +25,21 @@ namespace Cardificer{
         // A local reference to the current room, observes current room changes
         private Room localCurrentRoom;
 
+        // For debugging: different rooms have different colors
+        Color[] listOfColors;
 
-        private void Awake()
+        /// <summary>
+        /// Assign variables
+        /// </summary>
+        private void Start()
         {
-            if (roomImageContainer == null)
-            {
-                roomImageContainer = GetComponentInChildren<GridLayoutGroup>();
-            }
+            listOfColors = new Color[] { Color.red, Color.green, Color.blue };
         }
 
+        /// <summary>
+        /// Observe if the current room changes,
+        /// if it does, reset and update the map
+        /// </summary>
         private void Update()
         {
             if (localCurrentRoom != FloorGenerator.currentRoom)
@@ -42,74 +49,62 @@ namespace Cardificer{
             }
         }
 
+        /// <summary>
+        /// Completely remove all room visuals from the map:
+        /// wipe the map clean
+        /// </summary>
         private void ResetMap()
         {
             foreach(Transform child in roomImageContainer.transform)
             {
-                child.gameObject.GetComponent<Image>().sprite = blankRoomSprite;
+                Destroy(child.gameObject);
             }
         }
 
+        /// <summary>
+        /// instantiate room visual prefab (images)
+        /// depending on where a room is relative to the current
+        /// room the player is in
+        /// </summary>
         public void UpdateMap()
         {
-            print("Updating map");
+            // Reassign the current room
             localCurrentRoom = FloorGenerator.currentRoom;
+            
+            // Get a list of neighbor rooms
+            List<Room> neighborRooms = FloorGenerator.currentRoom.GetNeighboringRooms(FloorGenerator.map.map);
 
-            List<MapCell> roomEdges = FloorGenerator.currentRoom.GetEdgeCells(FloorGenerator.map.map);
-            foreach(MapCell cell in roomEdges)
+            // Update the current room by drawing all it's cells
+            foreach (MapCell cell in localCurrentRoom.GetRoomCells(FloorGenerator.map.map))
             {
-                print(cell.room.generated);
+                Vector2 drawLocation = cell.location - localCurrentRoom.roomLocation;
+                var roomVisual = Instantiate(roomVisualPrefab, roomImageContainer.transform);
+                roomVisual.transform.localPosition = new Vector2(roomVisual.transform.localPosition.x + (drawLocation.x * 100), roomVisual.transform.localPosition.y + (drawLocation.y * 100));
+                roomVisual.GetComponentInChildren<TextMeshProUGUI>().text = localCurrentRoom.roomType.displayName;
             }
 
-            roomImageContainer.gameObject.transform.GetChild(4).GetComponent<Image>().sprite = visitedRoomSprite;
-
-            for (int i = 0; i < roomEdges.Count; i++)
+            // Draw all cells of neighboring rooms
+            foreach (Room room in neighborRooms)
             {
-                if (roomEdges[i].direction.HasFlag(Direction.Up)) // Up
+                int index = Random.Range(0, listOfColors.Length);
+                Color randColor = listOfColors[index];
+                foreach(MapCell cell in room.GetRoomCells(FloorGenerator.map.map))
                 {
-                    if (roomEdges[i].room.generated)
+                    Vector2 drawLocation = cell.location - localCurrentRoom.roomLocation;
+                    var roomVisual = Instantiate(roomVisualPrefab, roomImageContainer.transform);
+                    roomVisual.transform.localPosition = new Vector2(roomVisual.transform.localPosition.x + (drawLocation.x * 100), roomVisual.transform.localPosition.y + (drawLocation.y * 100));
+                    roomVisual.GetComponent<Image>().color = randColor;
+                    if (!room.generated)
                     {
-                        roomImageContainer.gameObject.transform.GetChild(1).GetComponent<Image>().sprite = visitedRoomSprite;
+                        roomVisual.GetComponentInChildren<TextMeshProUGUI>().text = room.roomType.displayName;
                     }
                     else
                     {
-                        roomImageContainer.gameObject.transform.GetChild(1).GetComponent<Image>().sprite = nonVisitedRoomSprite;
-                    }
-                }
-                if (roomEdges[i].direction.HasFlag(Direction.Left)) // Left
-                {
-                    if (roomEdges[i].room.generated)
-                    {
-                        roomImageContainer.gameObject.transform.GetChild(3).GetComponent<Image>().sprite = visitedRoomSprite;
-                    }
-                    else
-                    {
-                        roomImageContainer.gameObject.transform.GetChild(3).GetComponent<Image>().sprite = nonVisitedRoomSprite;
-                    }
-                }
-                if (roomEdges[i].direction.HasFlag(Direction.Right)) // Right
-                {
-                    if (roomEdges[i].room.generated)
-                    {
-                        roomImageContainer.gameObject.transform.GetChild(5).GetComponent<Image>().sprite = visitedRoomSprite;
-                    }
-                    else
-                    {
-                        roomImageContainer.gameObject.transform.GetChild(5).GetComponent<Image>().sprite = nonVisitedRoomSprite;
-                    }
-                }
-                if (roomEdges[i].direction.HasFlag(Direction.Down)) // Down
-                {
-                    if (roomEdges[i].room.generated)
-                    {
-                        roomImageContainer.gameObject.transform.GetChild(7).GetComponent<Image>().sprite = visitedRoomSprite;
-                    }
-                    else
-                    {
-                        roomImageContainer.gameObject.transform.GetChild(7).GetComponent<Image>().sprite = nonVisitedRoomSprite;
+                        roomVisual.GetComponent<Image>().sprite = nonVisitedRoomSprite;
                     }
                 }
             }
+
         }
     }
 }
