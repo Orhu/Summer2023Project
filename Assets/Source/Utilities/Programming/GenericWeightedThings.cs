e01d62f9a9fca315e066acc2c985c661cd08616d
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-07-18 11:55:02 -0600

    even better generic weighted thing drawer

--- Assets/Source/Utilities/Programming/GenericWeightedThings.cs
+++ Assets/Source/Utilities/Programming/GenericWeightedThings.cs
@@ -11,7 +11,7 @@ namespace Cardificer
     /// </summary>
     /// <typeparam name="T"> The type of thing </typeparam>
     [System.Serializable]
-    public class GenericWeightedThing<T>
+    public class GenericWeightedThing<T> : GenericWeightedThingParent
     {
         [Tooltip("The weighted thing")]
         public T thing;
@@ -256,39 +256,104 @@ namespace Cardificer
     }
 
     /// <summary>
-    /// This is only to allow for the workaround of having the same property drawer for every generic weighted thing (thanks Unity)
+    /// This is only to allow for the workaround of having the same property drawer for every generic weighted things (thanks Unity)
     /// </summary>
     public class GenericWeightedThingsParent {};
 
-#if UNITY_EDITOR
     /// <summary>
-    /// Class for making the Generic weighted thing easier to read in the inspector.
+    /// This is only to allow for the workaround of having the same property drawer for every generic weighted thing (thanks Unity)
+    /// </summary>
+    public class GenericWeightedThingParent {};
+
+#if UNITY_EDITOR
+
+    /// <summary>
+    /// Class for making the Generic weighted things easier to read in the inspector.
     /// </summary>
     [CustomPropertyDrawer(typeof(GenericWeightedThingsParent), true)]
-    public class GenericWeightedThingDrawer : PropertyDrawer
+    public class GenericWeightedThingsDrawer : PropertyDrawer
     {
         /// <summary>
-        /// IDK HOW THIS WORKS DON'T @ ME
+        /// Draws the generic weighted thing property
         /// </summary>
-        /// <param name="position"></param>
-        /// <param name="property"></param>
-        /// <param name="label"></param>
+        /// <param name="position"> The position the property begins at </param>
+        /// <param name="property"> The property being drawn </param>
+        /// <param name="label"> The label of this property </param>
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
             EditorGUI.BeginProperty(position, label, property);
-            SerializedProperty things = property.FindPropertyRelative("things");
-            EditorGUI.PropertyField(position, things, label, true);
+            EditorGUI.PropertyField(position, property.FindPropertyRelative("things"), label, true);
+            EditorGUI.EndProperty();
         }
 
         /// <summary>
-        /// Overrides the height of the property
+        /// Gets the height this property should have
         /// </summary>
-        /// <param name="property"> The property dude idk </param>
-        /// <param name="label"> The label </param>
-        /// <returns> the property height??? </returns>
+        /// <param name="property"> The property </param>
+        /// <param name="label"> The label of this property </param>
+        /// <returns> The height of this property </returns>
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
             return EditorGUI.GetPropertyHeight(property.FindPropertyRelative("things"), true);
+        }
+    }
+
+    /// <summary>
+    /// Class for making the Generic weighted thing easier to read in the inspector.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(GenericWeightedThingParent), true)]
+    public class GenericWeightedThingDrawer : PropertyDrawer
+    {
+        /// <summary>
+        /// Draws the generic weighted thing property
+        /// </summary>
+        /// <param name="position"> The position the property begins at </param>
+        /// <param name="property"> The property being drawn </param>
+        /// <param name="label"> The label of this property </param>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            SerializedProperty thing = property.FindPropertyRelative("thing");
+            SerializedProperty weight = property.FindPropertyRelative("weight");
+            SerializedProperty maxChosen = property.FindPropertyRelative("maxChosen");
+
+            // + 2 To space the properties out a bit
+
+            position.height = EditorGUI.GetPropertyHeight(thing);
+            EditorGUI.PropertyField(position, thing, new GUIContent("Thing"), true);
+            position.y += EditorGUI.GetPropertyHeight(thing) + 2;
+
+            position.height = EditorGUI.GetPropertyHeight(weight);
+            EditorGUI.PropertyField(position, weight, new GUIContent("Weight"));
+            position.y += EditorGUI.GetPropertyHeight(weight) + 2;
+
+            position.height = EditorGUI.GetPropertyHeight(maxChosen);
+            EditorGUI.PropertyField(position, maxChosen, new GUIContent("Max Chosen"));
+            position.y += EditorGUI.GetPropertyHeight(maxChosen) + 2;
+
+            EditorGUI.EndProperty();
+        }
+
+        /// <summary>
+        /// Gets the height this property should have
+        /// </summary>
+        /// <param name="property"> The property </param>
+        /// <param name="label"> The label of this property </param>
+        /// <returns> The height of this property </returns>
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float height = 0;
+
+            SerializedProperty thing = property.FindPropertyRelative("thing");
+            SerializedProperty weight = property.FindPropertyRelative("weight");
+            SerializedProperty maxChosen = property.FindPropertyRelative("maxChosen");
+
+            height += EditorGUI.GetPropertyHeight(thing) + 2;
+            height += EditorGUI.GetPropertyHeight(weight) + 2;
+            height += EditorGUI.GetPropertyHeight(maxChosen) + 2;
+
+            return height;
         }
     }
 #endif
