684284182c3b01a6ed6a0f13450c5e49747215a6
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-07-19 09:58:57 -0600

    Add new actions
    Make split on death better

---
+++ Assets/Source/Enemies/FiniteStateMachine/Actions/StateVariables/ResetMaxSpeed.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections;
+using UnityEngine;
+using MovementType = Cardificer.RoomInterface.MovementType;
+
+namespace Cardificer.FiniteStateMachine
+{
+    /// <summary>
+    /// Represents an action that resets the max speed back to its default value.
+    /// </summary>
+    [CreateAssetMenu(menuName = "FSM/Actions/State Variables/Reset Max Speed")]
+    public class ResetMaxSpeed : SingleAction
+    {
+        /// <summary>
+        /// Resets the max speed back to its default value. 
+        /// </summary>
+        /// <param name="stateMachine"> The state machine to be used. </param>
+        /// <returns> Ends when the action is complete. </returns>
+        protected override IEnumerator PlayAction(BaseStateMachine stateMachine)
+        {
+            stateMachine.GetComponent<SimpleMovement>().maxSpeed = stateMachine.GetComponent<SimpleMovement>().originalMaxSpeed;
+            stateMachine.cooldownData.cooldownReady[this] = true;
+            yield break;
+        }
+    }
+}
