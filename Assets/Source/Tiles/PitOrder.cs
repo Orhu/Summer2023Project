27755f754b98b1d868f8200839dc437a9999e98d
Hexrin <71273371+Hexrin@users.noreply.github.com>
2023-07-22 16:24:03 -0600

    Created pit order asset

--- Assets/Source/Tiles/PitOrder.cs
+++ Assets/Source/Tiles/PitOrder.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 /// <summary>
 /// Maps ints to other ints
 /// </summary>
+[System.Serializable] [CreateAssetMenu(fileName = "NewPitOrder", menuName = "Pits/PitOrder")]
 public class PitOrder : ScriptableObject
 {
     // The a list of ints that will map one int to another int (values of -1 are unused)
