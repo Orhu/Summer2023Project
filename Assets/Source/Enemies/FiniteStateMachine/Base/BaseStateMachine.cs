a83728be9624e74db01c2509410e5cc12f659940
zakispie <93385173+zakispie@users.noreply.github.com>
2023-07-23 12:30:25 -0400

    Added directional arrow
    Simply attach it to any enemy you want, and they will have an arrow displayed in their forward-facing direction. Simply attach the DirectionalArrow component to the enemy.

--- Assets/Source/Enemies/FiniteStateMachine/Base/BaseStateMachine.cs
+++ Assets/Source/Enemies/FiniteStateMachine/Base/BaseStateMachine.cs
@@ -258,7 +258,6 @@ namespace Cardificer.FiniteStateMachine
             }
 
             currentState.OnStateUpdate(this);
-            print(gameObject.name + "'s Current State: " + currentState);
         }
 
         /// <summary>
