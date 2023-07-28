88b6784f28290d9bdf6311095a9d1cbf551c6b4d
zakispie <93385173+zakispie@users.noreply.github.com>
2023-07-28 13:56:52 -0400

    Aura Sphere particles

--- Assets/Source/Enemies/FiniteStateMachine/FloorBosses/LostCreature/LostCreature_FigureEightPath.cs
+++ Assets/Source/Enemies/FiniteStateMachine/FloorBosses/LostCreature/LostCreature_FigureEightPath.cs
@@ -11,7 +11,7 @@ namespace Cardificer.FiniteStateMachine
     /// Represents an action that draws some number of cards and adds them to the state machine.
     /// </summary>
     [CreateAssetMenu(menuName = "FSM/Floor Boss/Lost Creature/Set Path To Figure8 Or Infinity")]
-    public class LostCreature_SetPathTo : SingleAction
+    public class LostCreature_FigureEightPath : SingleAction
     {
         [Tooltip("Center point of the path shape, as an offset from the room's center")]
         [SerializeField] private Vector2 centerOffset;
@@ -36,13 +36,11 @@ namespace Cardificer.FiniteStateMachine
         }
 
         /// <summary>
-        /// Assumes the figure 8 or infinity path will be within a 29x4 or 4x29 bounding box. Generates a list of Vector2's needed to draw the path for the requested path type.
+        /// Generates a list of Vector2's needed to draw the path for the requested path type.
         /// </summary>
         /// <returns> List of Vector2 that make either an infinity or figure 8 path around the center position. </returns>
         private Vector2[] FormulatePath()
         {
-            float quarterHoriz = (float)shapeBounds.x / 4;
-            float quarterVert = (float)shapeBounds.y / 4;
             Vector2 roomCenter = RoomInterface.instance.myWorldPosition;
             List<Vector2> points = new List<Vector2>();
 
@@ -54,10 +52,7 @@ namespace Cardificer.FiniteStateMachine
                 for (int i = 0; i <= shapePoints; i++)
                 {
                     var x = ((float)i / shapePoints) * (2 * Mathf.PI);
-                    BaseStateMachine.print("x: " + x);
                     var vecX = constantX * Mathf.Sin(x);
-                    BaseStateMachine.print("Sin(x): " + vecX);
-                    BaseStateMachine.print("Cos(x): " + Mathf.Cos(x));
                     var vecY = constantY * Mathf.Sin(x) * Mathf.Cos(x);
                     points.Add(roomCenter + centerOffset + new Vector2(vecX, vecY));
                 }
@@ -70,28 +65,11 @@ namespace Cardificer.FiniteStateMachine
                 for (int i = 0; i <= shapePoints; i++)
                 {
                     var x = ((float)i / shapePoints) * (2 * Mathf.PI);
-                    BaseStateMachine.print("x: " + x);
                     var vecX = constantX * Mathf.Sin(x) * Mathf.Cos(x);
-                    BaseStateMachine.print("Sin(x): " + vecX);
-                    BaseStateMachine.print("Cos(x): " + Mathf.Cos(x));
                     var vecY = constantY * Mathf.Sin(x);
                     points.Add(roomCenter + centerOffset + new Vector2(vecX, vecY));
                 }
             }
-
-            /*
-                // Left Circle
-                points.Add(roomCenter + new Vector2(centerOffset.x - quarterHoriz, centerOffset.y + quarterVert));
-                points.Add(roomCenter + new Vector2(centerOffset.x - 2 * quarterHoriz, centerOffset.y));
-                points.Add(roomCenter + new Vector2(centerOffset.x - quarterHoriz, centerOffset.y - quarterVert));
-
-                // Right Circle
-                points.Add(roomCenter + new Vector2(centerOffset.x + quarterHoriz, centerOffset.y + quarterVert));
-                points.Add(roomCenter + new Vector2(centerOffset.x + 2 * quarterHoriz, centerOffset.y));
-                points.Add(roomCenter + new Vector2(centerOffset.x + quarterHoriz, centerOffset.y - quarterVert));
-
-                // Center
-                points.Add(roomCenter + centerOffset);*/
             return points.ToArray();
         }
     }
