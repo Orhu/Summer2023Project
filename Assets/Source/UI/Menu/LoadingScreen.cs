edfa18d0807cae378ca74bfd89e2b25d515868e9
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-07-31 16:12:16 -0600

    Update LoadingScreen.cs

--- Assets/Source/UI/Menu/LoadingScreen.cs
+++ Assets/Source/UI/Menu/LoadingScreen.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 namespace Cardificer
 {
+    /// <summary>
+    /// A loading screen for hiding async function calls.
+    /// </summary>
     public class LoadingScreen : MonoBehaviour
     {
         // The minimum time that this will load for.
