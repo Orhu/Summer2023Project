e02990f5d7ab2992d678802baec6f3e2c8c63231
Liam Healey <56327428+LiamDHealey@users.noreply.github.com>
2023-08-11 09:47:52 -0600

    Double hit delay

--- Assets/Source/Actions/Attack/AttackModifiers/Other/DoubleHit.cs
+++ Assets/Source/Actions/Attack/AttackModifiers/Other/DoubleHit.cs
@@ -10,28 +10,33 @@ namespace Cardificer
     [CreateAssetMenu(fileName = "NewDoubleHit", menuName = "Cards/AttackModifers/DoubleHit")]
     public class DoubleHit : AttackModifier
     {
+        [Tooltip("The time until the second hit is applied in seconds")] [Min(0)]
+        [SerializeField] private float delay = 0.5f;
+        
+
         // The projectile that will hit again.
         private Projectile projectile;
+
+        // The object that runs the double hit coroutines.
+        private static MonoBehaviour _routineRunner;
+        private static MonoBehaviour routineRunner
+        {
+            get
+            {
+                if (_routineRunner == null)
+                {
+                    _routineRunner = new GameObject("DoubleHitRoutineRunner").AddComponent<Empty>();
+                }
+
+                return _routineRunner;
+            }
+        }
 
         // Binds on overlap
         public override void Initialize(Projectile value)
         {
             projectile = value;
-            projectile.onOverlap +=
-                (Collider2D collider) =>
-                {
-                    value.StartCoroutine(SetBindings());
-                };
-            projectile.onOverlap += HitAgain;
-        }
 
-        /// <summary>
-        /// Rebinds hit again after a frame.
-        /// </summary>
-        /// <returns> Wait one frame. </returns>
-        private IEnumerator SetBindings()
-        {
-            yield return null;
             projectile.onOverlap += HitAgain;
         }
 
@@ -41,14 +46,49 @@ namespace Cardificer
         /// <param name="collider"> The object that was hit. </param>
         private void HitAgain(Collider2D collider)
         {
-            projectile.onOverlap -= HitAgain;
-            projectile.onOverlap?.Invoke(collider);
+            System.Action<Collider2D> overlapEffects = null;
+            int numOtherDoubleHits = 0;
+            int? index = null;
+            foreach (System.Delegate method in projectile.onOverlap.GetInvocationList())
+            {
+                if (method.Target.GetType() == GetType())
+                {
+                    if (this == method.Target)
+                    {
+                        index = numOtherDoubleHits + 1;
+                    }
+                    else
+                    {
+                        numOtherDoubleHits++;
+                    }
+                }
+                else
+                {
+                    overlapEffects += method as System.Action<Collider2D>;
+                }
+            }
 
             Health hitHealth = collider.gameObject.GetComponent<Health>();
-            if (hitHealth != null && projectile.applyDamageOnHit)
+            bool applyDamage = hitHealth != null && projectile.applyDamageOnHit;
+
+            DamageData damageData = new DamageData(projectile.attackData, projectile.attackData.causer);
+
+            routineRunner.StartCoroutine(DelayedHit());
+            IEnumerator DelayedHit()
             {
-                hitHealth.ReceiveAttack(projectile.attackData);
+                yield return new WaitForSeconds(delay * index.Value);
+                
+                if (collider == null) { yield break; }
+                overlapEffects?.Invoke(collider);
+
+                if (applyDamage)
+                {
+                    hitHealth.ReceiveAttack(damageData);
+                }
+
             }
         }
+
+        private class Empty : MonoBehaviour { }
     }
 }
