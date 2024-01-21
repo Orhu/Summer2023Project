a558332a700b860b499467d9292dfd894c2ff601
Emmeline <90860955+emmelinee@users.noreply.github.com>
2024-01-20 22:05:35 -0500

    Implemented set of enemy animations
    Finished bat, rat, spear thrower & hobgobbo. Lots of file moving bc finished enemies were taken out of the rig development folder

--- Assets/Content/Enemies/Development_Rigs/FlipStateBehaviour.cs
+++ Assets/Content/Enemies/Development_Rigs/FlipStateBehaviour.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 //handles characters that must flip AFTER a flipping animation plays.
 //add this behaviour to flipping animation state to flip the entire object on exit
 public class FlipStateBehaviour : StateMachineBehaviour
-{ 
+{
+    //the entire prefab we want to flip
     private GameObject Obj;
+
+    //the sprite we want to flip
+    private GameObject SpriteObj;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        SpriteObj = animator.gameObject;
+        Obj = SpriteObj.transform.parent.gameObject;
+
+        //"un-flip" sprites for duration of flipping animation
+        SpriteObj.transform.localScale = new Vector3((SpriteObj.transform.localScale.x * -1), SpriteObj.transform.localScale.y, SpriteObj.transform.localScale.z);
+
+        //flip parent prefab
+        Obj.transform.localScale = new Vector3((Obj.transform.localScale.x * -1), Obj.transform.localScale.y, Obj.transform.localScale.z);
+    }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Obj = animator.gameObject;
+        SpriteObj = animator.gameObject;
 
-        //flip on x axis
-        Obj.transform.localScale = new Vector3((Obj.transform.localScale.x *-1), Obj.transform.localScale.y, Obj.transform.localScale.z);
+        //finish by "re-flipping" sprites 
+        SpriteObj.transform.localScale = new Vector3((SpriteObj.transform.localScale.x * -1), SpriteObj.transform.localScale.y, SpriteObj.transform.localScale.z);
     }
 }
